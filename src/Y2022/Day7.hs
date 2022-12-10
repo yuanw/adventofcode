8b664650d4b4d01a2e97deb4594cf0d0f76619e3
yuanw <me@yuanwang.ca>
2022-12-10 15:20:27 -0600

    at my utter confusion on zipper

--- src/Y2022/Day7.hs
+++ src/Y2022/Day7.hs
@@ -1,45 +1,50 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Y2022.Day7 where
-
+import Data.List (break)
 import Data.Attoparsec.Text hiding (take)
 import Data.Text qualified as T
 import Control.Applicative (many, (<|>))
-data FileSystem = Directory {   _dName :: String, _content :: [FileSystem] } | File { _fName :: String , _size :: Int }
+import Data.Either (fromRight)
+data FileSystem = Directory {   _dName :: String, _content :: [FileSystem] } | File { _fName :: String , _size :: Int } deriving stock Show
 
 getSize :: FileSystem -> Int
 getSize (File _ s ) = s
 getSize (Directory _ []  ) = 0
 getSize (Directory n (x:xs)  ) = getSize x + getSize (Directory n xs  )
+type Name = String
+type Size = Int
+data Output = CD String | LS | Dir String | FL Int String deriving stock Show
 
-data Output = CD String | LS | Dir String | FL Int String deriving Show
-data Zipper = Zipper [FileSystem] [FileSystem]
-
-empty :: Zipper
-empty = Zipper [] []
+data FSCrumb = FSCrumb { _parent :: Name, _before :: [FileSystem] , _after :: [FileSystem]} deriving stock Show
+type Zipper = (FileSystem, [FSCrumb])
 
 parserFile :: Parser Output
 parserFile = do
   size <- many1 digit
-  space
+  _ <- space
   name <- many1 (letter <|> char '.' <|> digit)
   return $ FL (read size)  name
 
 outputParser :: Parser Output
 -- outputParser = (string "$ ls" >> return LS) <|> (string "$ cd " >>  CD <$> manyTill anyChar (string "\n") )  <|> (string "dir " >>  Dir <$> manyTill anyChar (string "\n") ) <|> parserFile
-
 outputParser = (string "$ ls" >> return LS) <|> (string "$ cd " >>  CD  <$> many (letter <|> char '/' <|> char '.' ))  <|> (string "dir " >>  Dir  <$> many (letter <|> digit)) <|> parserFile
 
 outputsParser :: Parser [Output]
 outputsParser = many (outputParser <* endOfLine   )
 
+empty :: Zipper
+empty = (Directory "/" [], [FSCrumb "/" [] []])
+
 test :: IO ()
 test = do
 
-    input <- readFile "data/2022/day7.txt"
-    print   ( length .lines $ input)
-    -- print $ map (parseOnly outputParser . T.pack) $ lines input
-    print $ fmap length . parseOnly outputsParser $ T.pack input
-
+    -- input <- readFile "data/2022/day7-test.txt"
+    -- let outputs = fromRight [] $ parseOnly outputsParser $ T.pack input
+    --     tree =  foldl buildTree empty (tail outputs)
+    -- print tree
+    let tree = foldl buildTree (Directory "a" [], [FSCrumb "/" [] []]) [ FL 14 "b.txt", FL 85 "c.dat", Dir "d" , CD "a", LS ] --, Dir "e"]
+    print tree
     -- print $ parseOnly outputParser "$ cd /"
     -- print $ parseOnly outputParser "$ ls"
     -- print $ parseOnly outputParser "14779 cmss"
@@ -47,10 +52,24 @@ test = do
     -- print $ parseOnly outputParser "101350 gpbswq.njr"
     -- print $ parseOnly outputParser "270744 mglrchsr"
 
--- zipper left maybe directory ,
--- buildTree :: Zipper ->  Output -> Zipper
--- buildTree (Zipper [] []) (CD n) = Zipper [Directory n [] ] []
--- buildTree z LS = z
--- buildTree (Zipper dirs items) (Dir n) = Zipper dirs (Directory n []  : items )
--- buildTree (Zipper dirs items) (FL s n) = Zipper dirs (File n s  : items )
--- buildTree (Zipper (Directory n' item' :xs) items) (CD n) =
+buildTree :: Zipper ->  Output -> Zipper
+buildTree z LS = z
+buildTree (item , FSCrumb name ls rs : bs) (Dir folderName) = (Directory folderName [], FSCrumb name (ls ++ [item]) rs  :bs)
+buildTree (item, FSCrumb name ls rs : bs) (FL size fileName) = (File fileName size, FSCrumb name (ls ++ [item]) rs : bs)
+buildTree z (CD "..") = goUp z
+buildTree z (CD name) = goTo name z
+buildTree _ _ = error ""
+
+goTo :: String -> Zipper -> Zipper
+goTo name (item, FSCrumb folderName ls rs : bs) = let (ls', rs') = break (nameIs name) (ls ++ (item : rs)) in (head rs', FSCrumb name ls' (tail rs') : FSCrumb folderName ls rs : bs)
+
+goUp :: Zipper -> Zipper
+goUp (item, FSCrumb name ls rs : bs) = (Directory name (ls ++ item : rs), bs)
+
+nameIs :: String -> FileSystem -> Bool
+nameIs name (Directory folderName _) = name == folderName
+nameIs name (File fileName _ ) = name == fileName
+
+topMost :: Zipper -> FileSystem
+topMost (item, [ FSCrumb name ls rs  ]) = Directory name (ls ++ item : rs)
+topMost z = topMost ( goUp z)
