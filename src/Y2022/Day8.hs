16914824de9cfe5834bb79cf52ca69836da44eea
yuanw <me@yuanwang.ca>
2022-12-11 17:42:01 -0600

    start with day 8

---
+++ src/Y2022/Day8.hs
@@ -0,0 +1,11 @@
+module Y2022.Day8 where
+
+type Grid = [[Int]]
+type Point = (Int, Int)
+
+isVisibleFromLeft :: Point -> Grid -> Bool
+isVisibleFromLeft = undefined
+
+
+isVisibleFromTop :: Point -> Grid -> Bool
+isVisibleFromTop = undefined
