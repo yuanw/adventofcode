592fe030838a20fa8e38203038fad8f1f0d829b3
yuanwang <yuan.wang@workiva.com>
2022-12-12 20:28:00 -0600

    wip

--- src/Y2022/Day8.hs
+++ src/Y2022/Day8.hs
@@ -6,7 +6,7 @@ type Point = (Int, Int)
 
 
 get :: Point -> Grid -> Int
-get (x,y) =  (!! y) .(!! x)
+get (x,y) =  (!! x) .(!! y)
 
 getWidth :: Grid -> Int
 getWidth = length . (!! 0)
@@ -15,27 +15,38 @@ getLength :: Grid -> Int
 getLength = length
 
 isVisibleFromLeft :: Point -> Grid -> Bool
-isVisibleFromLeft (0, _) _ = True
-isVisibleFromLeft (x,y) grid =  (isVisibleFromLeft (x-1, y) grid) && (get (x-1, y) grid < get(x,y) grid)
+isVisibleFromLeft (0,_) _ = True
+isVisibleFromLeft (x,y) grid = let val = get (x,y) grid in all (> val) [get (i, y) grid | i <- [ 0..x-1]]
+
+
+
 
 isVisibleFromTop :: Point -> Grid -> Bool
-isVisibleFromTop (0, _) _ = True
-isVisibleFromTop (x,y) grid =  (isVisibleFromTop (x, y-1) grid) && (get (x, y-1) grid < get(x,y) grid)
+isVisibleFromTop (_, 0) _ = True
+isVisibleFromTop (x,y) grid = let val = get (x,y) grid in all (> val) [get (x, i) grid | i <- [ 0..y-1]]
+
+
+
+
 
 isVisibleFromRight :: Point -> Grid -> Bool
-isVisibleFromRight (x, _) grid = x == getWidth grid
-isVisibleFromRight (x,y) grid =  (isVisibleFromTop (x+1, y) grid) && (get (x+1, y)   grid < get(x,y) grid)
+isVisibleFromRight (x,y) grid =   x == getWidth grid - 1 || let val = get (x,y) grid in all (> val) [get (i, y) grid | i <- [ x+1..getWidth grid]]
+
+
 
 
 isVisibleFromBottom :: Point -> Grid -> Bool
-isVisibleFromBottom (_, y) grid = y == getLength grid
-isVisibleFromBottom (x, y) grid =  (isVisibleFromBottom (x, y+1) grid) && (get (x, y +1)   grid < get(x,y) grid)
+isVisibleFromBottom (x, y) grid =  y == getLength grid - 1 ||  let val = get (x,y) grid in all (> val) [get (i, y) grid | i <- [ x+1..getWidth grid]]
+
+
+
 
 isVisible :: Point -> Grid -> Bool
 isVisible point grid = isVisibleFromLeft point grid || isVisibleFromRight point grid || isVisibleFromTop point grid || isVisibleFromBottom point grid
 
 countVisible :: Grid -> Int
-countVisible grid =  length [(x, y) | x  <- [0.. (getWidth grid) - 1 ], y <- [0.. (getLength grid) - 1], isVisible (x,y) grid]
+countVisible grid =  length [(x, y) | x  <- [0.. (getWidth grid) - 1 ],
+                             y <- [0.. (getLength grid) - 1], isVisible (x,y) grid]
 
 readInput :: FilePath -> IO Grid
 readInput file = map (map digitToInt ) . lines  <$> readFile file
@@ -43,5 +54,8 @@ readInput file = map (map digitToInt ) . lines  <$> readFile file
 testGrid :: IO Grid
 testGrid = readInput "data/2022/day8-test.txt"
 
-test :: IO Int
-test = countVisible <$> testGrid
+test :: IO ()
+test = do
+  grid <- testGrid
+  print  [isVisible (x, y) grid | x  <- [0.. (getWidth grid) - 1 ],
+                                  y <- [0.. (getLength grid) - 1]]
