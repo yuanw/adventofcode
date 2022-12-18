9e5236c27bda9c143f2893bde3765d865d80e34a
yuanwang <yuan.wang@workiva.com>
2022-12-18 13:03:05 -0600

    trying out part i

--- src/Y2022/Day11.hs
+++ src/Y2022/Day11.hs
@@ -3,4 +3,14 @@ module Y2022.Day11 where
 
 type Index = Int
 type Val = Int
-data Monkey = Monkey Index [Val] (Val -> Val) Val Index Index
+data Monkey = Monkey { _index :: Index, _items :: [Val], _op :: Val -> Val,
+                      _divisible :: Val, _trueThrow :: Index , _falseThrow :: Index}
+
+eval :: [Monkey] -> Monkey -> [Monkey]
+eval monkeys monkey = if null (_items monkey) then monkeys else undefined
+
+add :: [Monkey] -> Index -> Val -> [Monkey]
+add = undefined
+
+pop :: [Monkey] -> Monkey -> [Monkey]
+pop = undefined
