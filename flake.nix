69226d39af20454f30a2ef254a90454d3b5a7e40
yuanw <me@yuanwang.ca>
2022-12-25 15:23:02 -0600

    format

--- flake.nix
+++ flake.nix
@@ -13,24 +13,24 @@
       imports = [
         inputs.haskell-flake.flakeModule
         inputs.treefmt-nix.flakeModule
-       inputs.flake-root.flakeModule
+        inputs.flake-root.flakeModule
         inputs.mission-control.flakeModule
       ];
       perSystem = { self', lib, config, pkgs, ... }: {
-        haskellProjects.default = {
+        haskellProjects.main = {
           packages = {
             # You can add more than one local package here.
             aoc.root = ./.; # Assumes ./my-package.cabal
           };
 
-            buildTools = hp: {
+          buildTools = hp: {
             treefmt = config.treefmt.build.wrapper;
           } // config.treefmt.build.programs;
           # overrides = self: super: {}
           hlintCheck.enable = true;
           hlsCheck.enable = true;
         };
-             # source tree was auto formatted.
+        # source tree was auto formatted.
         treefmt.config = {
           inherit (config.flake-root) projectRootFile;
           package = pkgs.treefmt;
@@ -48,24 +48,41 @@
             ];
           };
         };
-     # source tree was auto formatted.
-        # treefmt.config = {
-        #   inherit (config.flake-root) projectRootFile;
-        #   package = pkgs.treefmt;
+        # Dev shell scripts.
+        mission-control.scripts = {
+          docs = {
+            description = "Start Hoogle server for project dependencies";
+            exec = ''
+              echo http://127.0.0.1:8888
+              hoogle serve -p 8888 --local
+            '';
+            category = "Dev Tools";
+          };
+          repl = {
+            description = "Start the cabal repl";
+            exec = ''
+              cabal repl "$@"
+            '';
+            category = "Dev Tools";
+          };
+          fmt = {
+            description = "Format the source tree";
+            exec = "${lib.getExe config.treefmt.build.wrapper}";
+            category = "Dev Tools ";
+          };
+          run = {
+            description = "Run the project with ghcid auto-recompile";
+            exec = ''
+              ghcid -c "cabal repl exe:haskell-template" --warnings -T :main
+            '';
+            category = "Primary";
+          };
+        };
 
-        #   programs.ormolu.enable = true;
-        #   programs.nixpkgs-fmt.enable = true;
-        #   programs.cabal-fmt.enable = true;
+        # Default shell.
+        devShells.default =
+          config.mission-control.installToDevShell self'.devShells.main;
 
-        #   # We use fourmolu
-        #   programs.ormolu.package = pkgs.haskellPackages.fourmolu;
-        #   settings.formatter.ormolu = {
-        #     options = [
-        #       "--ghc-opt"
-        #       "-XImportQualifiedPost"
-        #     ];
-        #   };
-        # };        # haskell-flake doesn't set the default package, but you can do it here.
         packages.default = self'.packages.aoc;
       };
     };
