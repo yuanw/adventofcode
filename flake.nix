cc3e80f42387f2b66479138da882dbd07f9477aa
yuanwang <yuan.wang@workiva.com>
2023-10-11 09:23:20 -0600

    update nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,11 @@
     haskell-flake.url = "github:srid/haskell-flake";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-root.url = "github:srid/flake-root";
-    mission-control.url = "github:Platonic-Systems/mission-control";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -14,22 +18,29 @@
         inputs.haskell-flake.flakeModule
         inputs.treefmt-nix.flakeModule
         inputs.flake-root.flakeModule
-        inputs.mission-control.flakeModule
       ];
       perSystem = { self', lib, config, pkgs, ... }: {
-        haskellProjects.main = {
-          packages = {
-            # You can add more than one local package here.
-            aoc.root = ./.; # Assumes ./my-package.cabal
+         haskellProjects.default = {
+          settings = { };
+          # overrides = self: super: { };
+          autoWire = [ "packages" "apps" "checks" ]; # Wire all but the devShell
+          devShell = {
+            hlsCheck.enable = false;
           };
-
-          buildTools = hp: {
-            treefmt = config.treefmt.build.wrapper;
-          } // config.treefmt.build.programs;
-          # overrides = self: super: {}
-          hlintCheck.enable = true;
-          hlsCheck.enable = true;
         };
+        # haskellProjects.main = {
+        #   packages = {
+        #     # You can add more than one local package here.
+        #     aoc.root = ./.; # Assumes ./my-package.cabal
+        #   };
+
+        #   buildTools = hp: {
+        #     treefmt = config.treefmt.build.wrapper;
+        #   } // config.treefmt.build.programs;
+        #   # overrides = self: super: {}
+        #   hlintCheck.enable = true;
+        #   hlsCheck.enable = true;
+        # };
         # source tree was auto formatted.
         treefmt.config = {
           inherit (config.flake-root) projectRootFile;
@@ -48,41 +59,46 @@
             ];
           };
         };
-        # Dev shell scripts.
-        mission-control.scripts = {
-          docs = {
-            description = "Start Hoogle server for project dependencies";
-            exec = ''
-              echo http://127.0.0.1:8888
-              hoogle serve -p 8888 --local
-            '';
-            category = "Dev Tools";
-          };
-          repl = {
-            description = "Start the cabal repl";
-            exec = ''
-              cabal repl "$@"
-            '';
-            category = "Dev Tools";
-          };
-          fmt = {
-            description = "Format the source tree";
-            exec = "${lib.getExe config.treefmt.build.wrapper}";
-            category = "Dev Tools ";
-          };
-          run = {
-            description = "Run the project with ghcid auto-recompile";
-            exec = ''
-              ghcid -c "cabal repl exe:haskell-template" --warnings -T :main
-            '';
-            category = "Primary";
-          };
-        };
+        # # Dev shell scripts.
+        # mission-control.scripts = {
+        #   docs = {
+        #     description = "Start Hoogle server for project dependencies";
+        #     exec = ''
+        #       echo http://127.0.0.1:8888
+        #       hoogle serve -p 8888 --local
+        #     '';
+        #     category = "Dev Tools";
+        #   };
+        #   repl = {
+        #     description = "Start the cabal repl";
+        #     exec = ''
+        #       cabal repl "$@"
+        #     '';
+        #     category = "Dev Tools";
+        #   };
+        #   fmt = {
+        #     description = "Format the source tree";
+        #     exec = "${lib.getExe config.treefmt.build.wrapper}";
+        #     category = "Dev Tools ";
+        #   };
+        #   run = {
+        #     description = "Run the project with ghcid auto-recompile";
+        #     exec = ''
+        #       ghcid -c "cabal repl exe:haskell-template" --warnings -T :main
+        #     '';
+        #     category = "Primary";
+        #   };
+        # };
 
         # Default shell.
-        devShells.default =
-          config.mission-control.installToDevShell self'.devShells.main;
+        devShells.default = pkgs.mkShell {
+ inputsFrom = [
+        config.treefmt.build.devShell
+        config.pre-commit.devShell
+        config.haskellProjects.default.outputs.devShell
+      ];
 
+      };
         packages.default = self'.packages.main-aoc;
       };
     };
