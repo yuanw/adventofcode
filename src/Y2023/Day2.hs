b461c320e58bb4d5ae229460f04927c34d2287da
yuanwang <yuan.wang@workiva.com>
2023-12-05 07:17:44 -0600

    day

--- src/Y2023/Day2.hs
+++ src/Y2023/Day2.hs
@@ -2,11 +2,14 @@
 
 module Y2023.Day2 where
 
-import Control.Applicative ((<|>))
+import Control.Applicative (many, (<|>))
 import Data.Attoparsec.Text
-import Data.Map qualified as Map
+import Data.Either (fromRight)
+import Data.Map.Strict qualified as Map
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
 
-data Color = Red | Blue | Green deriving (Show)
+data Color = Red | Blue | Green deriving (Show, Eq, Ord)
 
 data Game = Game {id :: Int, bags :: Map.Map Color Int} deriving (Show)
 
@@ -15,7 +18,7 @@ data Config = Config {totalBlueCount :: Int, totalRedCount :: Int, totalGreenCou
 data Cube = Cube Int Color deriving (Show)
 
 foldCube :: [Cube] -> Map.Map Color Int
-foldCube = (\(Cube i c) m -> Map.insert c i m) Map.empty
+foldCube = foldr (\(Cube i c) m -> Map.insertWith (+) c i m) Map.empty
 
 cubeParser :: Parser Cube
 cubeParser = do
@@ -30,5 +33,14 @@ gameParser = do
     string "Game "
     i <- decimal
     char ':'
-    _ <- cubeParser `sepBy` char ';'
-    return $ Game i Map.empty
+    cubes <- cubeParser `sepBy` char ';'
+    return $ Game i (foldCube cubes)
+
+gamesParser :: Parser [Game]
+gamesParser = many $ gameParser <* endOfLine
+
+partI :: IO ()
+partI = do
+    input <- TIO.readFile "data/2023/day2.txt"
+    let games = fromRight [] (parseOnly gamesParser input)
+    print games
