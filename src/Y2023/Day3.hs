728364d2dbb37aa370d230ee62b30158fc3f238d
yuanwang <yuan.wang@workiva.com>
2023-12-09 03:20:05 -0600

    wip

--- src/Y2023/Day3.hs
+++ src/Y2023/Day3.hs
@@ -1,4 +1,23 @@
 module Y2023.Day3 where
 
-type Grid = [[Int]]
+import Data.Char (digitToInt, isDigit)
+
+type Grid = [[Char]]
 type Point = (Int, Int)
+
+data State = State {currentNum :: [Point], allNum :: [[Point]]}
+
+lookUp :: Point -> Grid -> Char
+lookUp (x, y) g = (g !! y) !! x
+
+appendChar :: Point -> Grid -> State -> State
+appendChar p g (State cN aL) = if isDigit c then State (cN ++ [p]) aL else (State [] (if null cN then aL else cN : aL))
+  where
+    c = lookUp p g
+
+partI :: IO ()
+partI = do
+    grid <- lines <$> readFile "data/2023/day3-test.txt"
+    let y = length grid
+        x = length (head grid)
+    print (x, y)
