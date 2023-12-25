6f92bbbf42029648966782b8bff4b5fe9722f78c
yuanwang <yuan.wang@workiva.com>
2023-12-25 07:11:22 -0600

    wip

--- src/Y2023/Day8.hs
+++ src/Y2023/Day8.hs
@@ -2,6 +2,7 @@ module Y2023.Day8 where
 
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as M
+import Data.Text.IO qualified as TIO
 
 data Direction = L | R
 
@@ -10,3 +11,7 @@ newtype Path = Path {getPath :: (Node, Node)}
 
 type Network = Map Node Path
 type Directions = [Direction]
+
+partI :: IO ()
+partI = do
+    input <- TIO.readFile "data/2023/day8-test.txt"
