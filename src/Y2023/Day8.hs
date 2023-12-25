f93c52adb1b5394a5f06b48f9416b7d3eb591c83
yuanwang <yuan.wang@workiva.com>
2023-12-25 07:38:42 -0600

    wip

--- src/Y2023/Day8.hs
+++ src/Y2023/Day8.hs
@@ -1,5 +1,7 @@
 module Y2023.Day8 where
 
+import Control.Applicative (many, (<|>))
+import Data.Attoparsec.Text
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as M
 import Data.Text.IO qualified as TIO
@@ -11,7 +13,42 @@ newtype Path = Path {getPath :: (Node, Node)}
 
 type Network = Map Node Path
 type Directions = [Direction]
+type Input = (Directions, Network)
+
+directionParser :: Parser Direction
+directionParser = (char 'R' >> return R) <|> (char 'L' >> return L)
+
+directionsParser :: Parser Directions
+directionsParser = many $ directionParser <* endOfLine
+
+nodeParser :: Parser (Node, Path)
+nodeParser = do
+    node <- takeTill (\x -> x != ' ')
+    skipSpace
+    char '='
+    skipSpace
+    char '('
+    l <- takeTill (\x -> x != ' ')
+    char ','
+    skipSpace
+    r <- takeTill (\x -> x != ' ')
+    char ')'
+    return $ (node, Path (l, r))
+
+nodesParser :: Parser [(Node, Path)]
+nodesParser = many $ nodeParser <* endOfLine
+
+inputParser :: Parser Input
+inputParser = do
+    ds <- directionsParser
+    skipSpace
+    nodes <- nodesParser
+    return $ (ds, M.fromList nodes)
 
 partI :: IO ()
 partI = do
-    input <- TIO.readFile "data/2023/day8-test.txt"
+    rawInput <- TIO.readFile "data/2023/day8-test.txt"
+    let e = parseOnly inputParser rawInput
+    case e of
+        Left err -> putStrLn $ "Error while parsing: " ++ err
+        Right input -> print input
