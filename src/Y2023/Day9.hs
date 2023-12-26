ac698b1ebf9a438965a72d05a682e7fc95ac2309
yuanwang <yuan.wang@workiva.com>
2023-12-26 11:16:24 -0600

    cheap impl of backward

--- src/Y2023/Day9.hs
+++ src/Y2023/Day9.hs
@@ -37,11 +37,16 @@ choose n 1 = n
 choose 0 _ = 0
 choose n k = choose (n - 1) (k - 1) * n `div` k
 
+-- https://www.geeksforgeeks.org/newton-forward-backward-interpolation/
 -- https://www.youtube.com/watch?app=desktop&v=4AuV93LOPcE&t=1679s
 forward :: Int -> [Int] -> Int
 forward n row = foldr (\(x, y) accum -> accum + x * (choose n y)) 0 factors
   where
     factors = zip row [0 ..] :: [(Int, Int)]
+
+-- https://atozmath.com/example/CONM/NumeInterPola.aspx?q=B&q1=E1
+findPrev :: [Int] -> Int
+findPrev = findNext . reverse
 
 findNext :: [Int] -> Int
 findNext numbers = forward (length numbers) factors
@@ -56,3 +61,11 @@ partI = do
     case e of
         Left err -> putStrLn $ "Error while parsing: " ++ err
         Right input -> print (sum $ map findNext input)
+
+partII :: IO ()
+partII = do
+    rawInput <- TIO.readFile "data/2023/day9.txt"
+    let e = parseOnly inputParser rawInput
+    case e of
+        Left err -> putStrLn $ "Error while parsing: " ++ err
+        Right input -> print (sum $ map findPrev input)
