688b86d9e0cf48d12235fe9288fa88194a76cc0c
yuanw <me@yuanwang.ca>
2024-12-11 18:12:52 -0600

    wip

--- src/Y2024/Day3.hs
+++ src/Y2024/Day3.hs
@@ -2,15 +2,20 @@
 
 module Y2024.Day3 where
 
+import Data.Text (Text)
+import Data.Void (Void)
 import Text.Megaparsec qualified as P
+import Text.Megaparsec.Char qualified as PC
 
-mulParser :: Parser (Int, Int)
+type StringParser = P.Parsec Void String
+
+mulParser :: StringParser (Int, Int)
 mulParser = do
-    _ <- string "mul("
-    a <- decimal
-    _ <- char ','
-    b <- decimal
-    _ <- string ")"
+    _ <- PC.string "mul("
+    a <- P.decimal
+    _ <- PC.char ','
+    b <- P.decimal
+    _ <- PC.string ")"
     return (a, b)
 
 -- P.try . P.skipManyTill P.anySingle . P.try
