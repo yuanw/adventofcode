61795a51969e62e584eb204401ab151cce8b3a3d
yuanwang <yuan.wang@workiva.com>
2025-12-01 12:11:39 -0600

    wip

--- src/Y2025/Day1.hs
+++ src/Y2025/Day1.hs
@@ -1,1 +1,8 @@
 module Y2025.Day1 where
+
+import Control.Arrow (second)
+import Data.List (uncons)
+import Data.Maybe (fromJust)
+
+readTuple :: String -> (Char, Int)
+readTuple = fromJust . fmap (second read) . uncons
